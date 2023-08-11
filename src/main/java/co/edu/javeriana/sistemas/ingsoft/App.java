87ebcaaaad1c74c47d0f35d7ac76f4422636ff30
Arduousbook <davids_barretom@javeriana.edu.co>
2023-08-11 02:27:59 -0500

    Upload of finished version of the program. - Finished version of the program in it's one class-version.

--- src/main/java/co/edu/javeriana/sistemas/ingsoft/App.java
+++ src/main/java/co/edu/javeriana/sistemas/ingsoft/App.java
@@ -1,5 +1,9 @@
 package co.edu.javeriana.sistemas.ingsoft;
 
+//import jdk.internal.net.http.common.Pair;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App
@@ -8,6 +12,7 @@ public class App
     {
         System.out.println("--------------------//------------------");
         System.out.println("Bievenido. A continación puede calcular su promedio ponderado.");
+        obtainUserInput();
 
 
     }
@@ -18,28 +23,46 @@ public class App
     2. Number of credits of that course.
     3. NUmber of total credits approved in the career.
      */
-    public void obtainUserInput(){
+    public static void obtainUserInput(){
 
         float courseGrade;
         int courseCredits;
         int totalCreditsApproved;
+        ArrayList<Pair<Float, Integer>> grade_Credits_Array = new ArrayList<>();
+        int creditsLoopCounter = 0;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nota obtenida en  la materia: ");
-        courseGrade = scanner.nextFloat();
-        System.out.println("Numero de créditos de la materia: ");
-        courseCredits = scanner.nextInt();
+
         System.out.println("Numero de creditos totales aprovados: ");
         totalCreditsApproved = scanner.nextInt();
 
-        calculateWeightedAverage();
+        while(creditsLoopCounter != totalCreditsApproved){
+            System.out.println("Nota obtenida en  la materia: ");
+            courseGrade = scanner.nextFloat();
+            System.out.println("Numero de créditos de la materia: ");
+            courseCredits = scanner.nextInt();
+
+            grade_Credits_Array.add(new Pair<>(courseGrade, courseCredits));
+            creditsLoopCounter += courseCredits;
+        }
+
+        float promedioPonderado =  calculateWeightedAverage(grade_Credits_Array, totalCreditsApproved);
+
+        System.out.println("Listos! El promedio ponderado es: " + promedioPonderado);
     }
 
-    public float calculateWeightedAverage(float courseGrade, int courseCredits, int totalCreditsApproved){
+    public static float calculateWeightedAverage(ArrayList<Pair<Float, Integer>> grades_CreditsArray, int totalCreditsApproved){
 
         float weightedAverage;
+        float denominator = 0;
 
-        weightedAverage = (courseCredits * courseCredits)/totalCreditsApproved;
+        for(Pair<Float, Integer> pair : grades_CreditsArray){
+            denominator += pair.getKey() * pair.getValue();
+            System.out.println(denominator);
+        }
+
+
+        weightedAverage = denominator/totalCreditsApproved;
 
         return weightedAverage;
     }
